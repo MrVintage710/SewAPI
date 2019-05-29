07cd57609fffb8a93efd26622c66bb453dc0a34d
BuildTools <unconfigured@null.spigotmc.org>
2019-05-28 18:01:52 -0600

    initial commit

---
+++ src/main/java/me/mrvintage/sew/SewMain.java
@@ -0,0 +1,11 @@
+package me.mrvintage.sew;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SewMain implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
